4802b8ba34cd92c1c4968af61ea1b7cce0c91d35
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-04-16 15:45:35 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -7,34 +7,31 @@ from helpers.filters import command, other_filters, other_filters2
 
 
 ## ~ Simple Config ~ ##
-FRIEND_BOT = "MusicsNexa_bot"
-USER_ACCNAME = os.getenv("USER_ACCNAME", "NexaMusicAssistant")
+FRIEND_BOT = "Achubiju6c"
+USER_ACCNAME = os.getenv("USER_ACCNAME", "Achubiju6c")
 
 
 
-@Client.on_message(command(["start", "start@TheNexasMusic_bot"]))
+@Client.on_message(command(["start", "start@YeageristMusic_bot"]))
 async def start(_, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
-I'm The Nexa Music Bot. Friend of **@{FRIEND_BOT}** 😏️.
-
+I'm TheYeagerist Music Streamer Bot. Friend of **@{FRIEND_BOT}** 😏️.
 I can play Music In Telegram Groups Via Voice Chat! 😌️.
-
-Made with ❤️ <b>@NexaBotsUpdates</b>""",
+Made with ❤️ <b>@Animemusicarchive6</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🤨️ How To Use Me 🤨️", url="https://telegra.ph/How-To-Use-Music-Nexa-Bot-03-16"
+                        "🤨️ How To Use Me 🤨️", url="https://telegra.ph/How-To-Use-Yeagerist-Music-Streamer-Bot-04-05"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "🔰️ My Update Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                        "🔰️ My Update Channel 🔰️", url="https://t.me/Animemusicarchive6"
                     ),
                     InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                        "⚜️ Support Group ⚜️", url="https://t.me/Yeageristbots"
                     )
                 ]
             ]
@@ -46,28 +43,23 @@ Made with ❤️ <b>@NexaBotsUpdates</b>""",
 async def help(_, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
-Bruh! Do you need Help! 🤔️ yea yea I know it! 🙃️
-
+Hi ! Do you need Help! 🤔️ yea yea I know it! 🙃️
 How To Use Me? 🧐️
-
 <b> 1. Add Me and @{USER_ACCNAME} To Your Group!
-
  2. Give Admin To Me and @{USER_ACCNAME} ! </b>
  
  Enjoy! 😌️
-
-Made with ❤️ <b>@NexaBotsUpdates</b>""",
+Made with ❤️ <b>@Animemusicarchive6</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔰️ My Update Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                        "🔰️ My Update Channel 🔰️", url="https://t.me/Animemusicarchive6"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                        "⚜️ Support Group ⚜️", url="https://t.me/Yeageristbots"
                     )
                 ]
             ]
@@ -75,13 +67,11 @@ Made with ❤️ <b>@NexaBotsUpdates</b>""",
     )
 
     
-@Client.on_message(command(["cmdlist", "cmdlist@TheNexasMusic_bot"]))
+@Client.on_message(command(["cmdlist", "cmdlist@YeageristMusic_bot"]))
 async def cmdlist(_, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
 Here is the list of available commands! 😃️
-
 <code>/play</code> - Reply to supported url or "/play supported url"
 <code>/skip</code> - Skip currenly playing song!
 <code>/pause</code> - Pause currently playing song!
@@ -101,12 +91,12 @@ Here is the list of available commands! 😃️
                 ],
                 [
                     InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                        "⚜️ Support Group ⚜️", url="https://t.me/Yeageristbots"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                    "🔰️ My Update Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                    "🔰️ My Update Channel 🔰️", url="https://t.me/Animemusicarchive6"
                     )
                 ]
             ]
@@ -118,27 +108,22 @@ Here is the list of available commands! 😃️
 async def credits2(_, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
 __Note!__ ⚠️: This Project Is <b>Not Fully Owned By Me</b> !
-
 Credits To,
-
 <b><a href="https://github.com/CallsMusic">CallsMusic</a></b> - For Callsmusic (Main Code ❤️) !
 <b><a href="https://github.com/nikhileashy">N A C</a></b> - For <code>/vc</code> Command
-
 Made with ❤️ by **@NexaBotsUpdates**
-
 Respect To Code Owners! Not To Me!""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔰️ My Update Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                        "🔰️ My Update Channel 🔰️", url="https://t.me/Animemusicarchive6"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                        "⚜️ Support Group ⚜️", url="https://t.me/Yeageristbots"
                     )
                 ]
             ]
@@ -146,29 +131,25 @@ Respect To Code Owners! Not To Me!""",
     )   
 
 
-@Client.on_message(command(["vc", "vc@TheNexasMusic_bot"]) & other_filters)
+@Client.on_message(command(["vc", "vc@YeageristMusic_bot"]) & other_filters)
 async def vc(_, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
-
              😌️  **Voice Chat Link** 😌️
 ____________________------------______________________
-
 👉️ https://t.me/{message.chat.username}?voicechat   👈️
 ____________________------------______________________
-
 Enjoy!😌️❤️""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔰️ My Update Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                        "🔰️ My Update Channel 🔰️", url="https://t.me/Animemusicarchive6"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                        "⚜️ Support Group ⚜️", url="https://t.me/Yeageristbots"
                     )
                 ]
             ]
@@ -176,7 +157,7 @@ Enjoy!😌️❤️""",
     )
 
     
-@Client.on_message(command(["search", "search@TheNexasMusic_bot"]))
+@Client.on_message(command(["search", "search@YeageristMusic_bot"]))
 async def search(_, message: Message):
     await message.reply_text(
         "💁🏻‍♂️ Do you want to search for a YouTube video?",
